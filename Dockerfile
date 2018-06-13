b65deb6f11b673bbda1856c3d99c7e363524fc05
geoffroygivry <geoff.givry@gmail.com>
2018-06-13 22:41:35 +0100

    updated Docker image with new python packages installed

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,11 @@ RUN apt-get upgrade -y
 
 RUN export DEBIAN_FRONTEND=noninteractive
 
-RUN apt-get install -y tzdata
+RUN apt-get install -y tzdata nano
 RUN ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 RUN dpkg-reconfigure --frontend noninteractive tzdata
 
 # Install pip, Python and matplotlib required libraries
 RUN apt-get update && apt-get install -y python python-dev python-tk python-pip \
-    libxft-dev libfreetype6 libfreetype6-dev
+    libxft-dev libfreetype6 libfreetype6-dev libmysqlclient-dev
 RUN pip install --no-cache-dir -r requirements.txt
--- test_imports.py
+++ test_imports.py
@@ -1,13 +1,21 @@
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS 
+import matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+import wordcloud
+print('WordCloud: %s' % wordcloud.__version__)
 import os
-from bs4 import BeautifulSoup
+import bs4
+print('BeautifulSoup: %s' % bs4.__version__)
 from collections import Counter
 from string import punctuation
 import re
 import requests
 import nltk
-import tornado.ioloop
-import tornado.web
+print('nltk: %s' % nltk.__version__)
+import tornado
+print('tornado: %s' % tornado.version)
+import torndb
+print('torndb: %s' % torndb.version)
+import MySQLdb
+print('MySQLdb: %s' % MySQLdb.__version__)
 
-print("looking great!")
+print("All imports succeeded. Well done!")
